137873f00032152bfae6659a75bd29bc89498701
martinzeroo <martinhobesalu@gmail.com>
2023-05-24 12:32:29 +0300

    Made chirp page

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Tracking;
 use Illuminate\Http\Request;
+use Illuminate\Http\View;
 
 class TrackingController extends Controller
 {
@@ -12,7 +13,7 @@ class TrackingController extends Controller
      */
     public function index()
     {
-        //
+        return view('Tracking.index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TrackingController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('Tracking', TrackingController::class)
+    -> middleware(['auth','verified']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
