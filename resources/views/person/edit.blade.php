f4a0a70da38d038d2a3dff6ba70d8ad101079910
Alkems <mynamezalter@gmail.com>
2023-06-09 12:46:37 +0300

    Fixed Edit for both Person and Project.

--- resources/views/person/edit.blade.php
+++ resources/views/person/edit.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-        <form method="POST" action="{{ route('people.update', $person) }}">
+        <form method="POST" action="{{ route('person.update', $Person) }}">
             @csrf
             @method('patch')
             <input type="text"
@@ -9,9 +9,14 @@
               placeholder="{{__('Name the person')}}"
               class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
            <input type="text"
-               name="project"
-               value="{{old('project', $Person -> project)}}"
-               placeholder="{{__('Name the project')}}"
+               name="fullname"
+               value="{{old('fullname', $Person -> fullname)}}"
+               placeholder="{{__('Fullname')}}"
+               class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
+            <input type="text"
+               name="identification"
+               value="{{old('identification', $Person -> identification)}}"
+               placeholder="{{__('Identification Code')}}"
                class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
             <div class="mt-4 space-x-2">
                 <x-primary-button>{{ __('Save') }}</x-primary-button>
--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,16 +1,16 @@
 <x-app-layout>
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-        <form method="POST" action="{{ route('projects.update', $project) }}">
+        <form method="POST" action="{{ route('projects.update', $Projects) }}">
             @csrf
             @method('patch')
             <input type="text"
             name="person"
-             value="{{old('person', $Project -> person)}}"
+             value="{{old('person', $Projects -> person)}}"
               placeholder="{{__('Name the person')}}"
               class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
            <input type="text"
                name="project"
-               value="{{old('project', $Project -> project)}}"
+               value="{{old('project', $Projects -> project)}}"
                placeholder="{{__('Name the project')}}"
                class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
             <div class="mt-4 space-x-2">
